f9b3425536315456bb4d621f90ab59acf7ae9557
Pirooz Wallace <PiroozWallace@outlook.com>
2020-12-14 16:05:58 -0800

    creating html routes
    routing

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+module.exports = function(app){
+
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+  })
+
+  app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+  })
+}
