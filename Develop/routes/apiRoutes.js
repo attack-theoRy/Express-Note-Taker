ae783f35dd39d30c733c114744eb457f097dbfc5
Pirooz Wallace <PiroozWallace@outlook.com>
2020-12-16 10:15:11 -0800

    new note functionality

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -22,3 +22,23 @@ app.get("/api/notes/:id", (req, res) => {
     return res.json(false)
 
 })
+
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    if(notes.length === 0){
+        newNote.id = 1 }
+    else {
+        newNote.id = (notes[notes.length-1].id + 1);
+    }
+    notes.push(newNote);
+    let jsonNotes = JSON.stringify(notes)
+    fs.writeFile("./db/db.json", jsonNotes, function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Success!");
+    })
+    res.json(true)
+  })
+
+}
--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,9 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+app.use(express.static('public'));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
