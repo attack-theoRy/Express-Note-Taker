d4ea1ef47eb9c58487dc4a081159c39955f307bd
Pirooz Wallace <PiroozWallace@outlook.com>
2020-12-16 13:44:04 -0800

    here I go again

--- Develop/server.js
+++ Develop/server.js
@@ -8,8 +8,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.static('public'));
 
-require("./Develop/routes/apiRoutes")(app);
-require("./Develop/routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log(`Server is listening port: ${PORT}`)
