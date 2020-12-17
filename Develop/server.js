f6c6ad42c359f8b25d2930abb63f7fb4ba04f76e
Pirooz Wallace <PiroozWallace@outlook.com>
2020-12-16 17:37:09 -0800

    heroku

--- Develop/server.js
+++ Develop/server.js
@@ -4,9 +4,9 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }))
+//app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
